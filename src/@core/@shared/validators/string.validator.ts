beb01640f2e7ca636334d80c59eddf5f50a8f33e
Flávio Nobre <flavionobredev@gmail.com>
2024-08-03 14:19:35 -0300

    Revert "fix: expose isUUID fn"
    This reverts commit 9bf993e761a53c877613131f0f78032145f5beda.

--- src/@core/@shared/validators/string.validator.ts
+++ src/@core/@shared/validators/string.validator.ts
@@ -6,9 +6,3 @@ export const isUrl = (url: string) => {
     return false;
   }
 };
-
-export function isUUID(uuid: string): boolean {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/.test(
-    uuid,
-  );
-}
