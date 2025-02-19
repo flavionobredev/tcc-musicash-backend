f5ccffdb2876c9190161447671e6a81d0ae28dea
Flávio Nobre <flavionobredev@gmail.com>
2025-02-19 18:09:55 -0300

    fix: improves min date validation

--- src/modules/events/dtos/create-default-event.dto.ts
+++ src/modules/events/dtos/create-default-event.dto.ts
@@ -21,12 +21,12 @@ export class CreateDefaultEventInputDTO {
   })
   type?: string;
 
-  // @IsString()
-  // @IsNotEmpty()
   @IsDate({ message: 'startDate must be a Date instance' })
   @MinDate(
     () => {
       const now = new Date();
+      now.setHours(0);
+      now.setMinutes(0);
       now.setSeconds(0);
       now.setMilliseconds(0);
       return now;
